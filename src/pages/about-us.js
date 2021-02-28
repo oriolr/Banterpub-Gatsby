8f5545173de123f263256a6bf76adc5e60e17013
Ama <ama@aningo.com>
2021-02-28 02:50:36 -0600

    Add an about us page.

---
+++ src/pages/about-us.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutUsPage = () => (
+<Layout>
+    <SEO title="About Us" />
+    <h1>About Us</h1>
+    <p>Neal Carter</p>
+    <p>Joseph Guthrie</p>
+    <p>Ama Aningo</p>
+    <p>Adam Carnegie</p>
+    <p>Jonathan Alingu</p>
+</Layout>
+
+)
+
+export default  AboutUsPage
